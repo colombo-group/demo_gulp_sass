39913d254745df441034b943e322aa7979728209
javis <dai12051997@gmail.com>
2016-09-21 15:55:28 +0700

    update

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,44 @@
+// var gulp = require('gulp');
+// var sass = require('gulp-sass');
+
+// gulp.task('default', function(){
+//   return gulp.src('sass/style.scss')
+//     .pipe(sass()) // Converts Sass to CSS with gulp-sass
+//     .pipe(gulp.dest('public/css'))
+// });
+// // gulp.task('',function(){
+	
+// // 	});
+
+
+
+
+
+// Include gulp
 var gulp = require('gulp');
+
+// Include Our Plugins
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var livereload = require('gulp-livereload');
+var minifyCss = require('gulp-cssnano');
 
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./public/css/'))
+// Compile Our Sass
+gulp.task('sass', function () {
+    return gulp.src('sass/style.scss')
+        .pipe(sass())
+        .pipe(minifyCss())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('public/css'))
+        .pipe(livereload());
 });
 
-//Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+// Watch Files For Changes
+gulp.task('watch', function () {
+    livereload.listen();
+    gulp.watch(['sass/**/*'], ['sass']);
 });
+
+// Default Task
+gulp.task('default', ['sass', 'watch']);
