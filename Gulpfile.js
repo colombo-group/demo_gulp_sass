6bdecdad8513f0cb81533b668c443c0538aaecd1
javis <dai12051997@gmail.com>
2016-09-22 17:35:06 +0700

    fix lan nua

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,7 +3,6 @@ var
     gulp = require('gulp'),
     sass = require('gulp-sass');
 // default task
-gulp.task('watch', function)
 gulp.task('default', function () {
      return gulp.src('src/scss/style.scss')
      .pipe(sass())
