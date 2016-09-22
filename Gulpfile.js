06b134d7e756403eb2f5476870d2245e927e6598
javis <dai12051997@gmail.com>
2016-09-22 16:59:32 +0700

    s

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,7 @@ var
     gulp = require('gulp'),
     sass = require('gulp-sass');
 // default task
+gulp.task('watch', function)
 gulp.task('default', function () {
      return gulp.src('src/scss/style.scss')
      .pipe(sass())
